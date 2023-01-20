14ba4a489078e6744720ecf43188daf317105231
ณภัทร จันทร์เมือง <naphatjm@gmail.com>
2023-01-20 11:51:40 +0700

    Add data to db

--- server-ts/src/index.ts
+++ server-ts/src/index.ts
@@ -2,13 +2,19 @@ import Koa from 'koa'
 import { koaBody } from 'koa-body'
 import cors from '@koa/cors'
 import apiRouter from './api'
+import appConfig from './config'
+import loadFixtures from './fixtures'
+import json from 'koa-json'
 
 const app = new Koa()
 
 app.use(cors());    
 app.use(koaBody());
+app.use(json())
+app.use(apiRouter.routes());
 
-app.use(apiRouter.routes())
-
-app.listen(3000)
-
+(async () => {
+    await loadFixtures(appConfig.clearDataBeforeLodingFixture)
+    //await initSsoCert()
+    app.listen(3000)
+  })();
