5bdd5f811e5cf50ea1b9482c40ec6050b1b2d6b0
ณภัทร จันทร์เมือง <naphatjm@gmail.com>
2023-01-21 23:48:33 +0700

    insert authMiddleware

--- server-ts/src/api/Result.ts
+++ server-ts/src/api/Result.ts
@@ -1,18 +1,20 @@
 import Router from "koa-router"
 import db from '../db'
+import { AuthData } from "../auth"
 
 const router = new Router()
 
-router
-    .get('/', async (ctx, next) => {
-        let query = db('user_result').select('*')
-        if (ctx.request.query['keyword']) {
-            const keyword = String(ctx.request.query['keyword'])
-            query = query.where((it) => {it.where('result','like', `%${keyword}`).orWhere('remark','like',`%${keyword}`)})
-        }
-        ctx.body = await query.orderBy('remark','desc')
 
-    })
+router.get('/', async (ctx, next) => {            
+    const authData = ctx.state.authData as AuthData
+    let query = db('user_result').select('*').where({ 'user_result.userCode': authData.username })
+    if (ctx.request.query['keyword']) {
+      const keyword = String(ctx.request.query['keyword'])
+      query = query.where((it) => {it.where('result', 'like', `%${keyword}%`).orWhere('remark', 'like', `%${keyword}%`)})
+    }
+    ctx.body = await query.orderBy('id', 'desc')
+  })
 
 
 export default router
+
--- server-ts/src/api/index.ts
+++ server-ts/src/api/index.ts
@@ -1,10 +1,11 @@
 import Router from 'koa-router'
 import announcement from './Announcement'
 import user_result from './Result'
+import { authMiddleware } from "../auth";
 
 const apiRouter = new Router()
 
-apiRouter.use('/api/announcement', announcement.routes())
-apiRouter.use('/api/userResult', user_result.routes())
+apiRouter.use('/api/announcement',authMiddleware, announcement.routes())
+apiRouter.use('/api/userResult',authMiddleware, user_result.routes())
 
 export default apiRouter
