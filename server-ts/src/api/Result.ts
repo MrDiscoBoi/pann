eb08ede1a8b06f504fdc9f83552526a117b724a1
ณภัทร จันทร์เมือง <naphatjm@gmail.com>
2023-01-22 16:43:36 +0700

    create nestobject

--- server-ts/src/api/Result.ts
+++ server-ts/src/api/Result.ts
@@ -1,20 +1,24 @@
-import Router from "koa-router"
-import db from '../db'
-import { AuthData } from "../auth"
+import Router from "koa-router";
 
+import db from "../db";
 const router = new Router()
 
-
-router.get('/', async (ctx, next) => {            
-    const authData = ctx.state.authData as AuthData
-    let query = db('user_result').select('*').where({ 'user_result.userCode': authData.username })
-    if (ctx.request.query['keyword']) {
+router
+.get('/', async (ctx,next) => {
+  let query = db('userResult').select('*')
+  if (ctx.request.query['announcementId']) {
+      const announcementId = Number(ctx.request.query['announcementId'])
+      query = query.where({announcementId})
+  }
+  if (ctx.request.query['isPinned']) {
+      const isPinned = Boolean(ctx.request.query['isPinned'])
+      query = query.where({ isPinned })
+  }
+  if (ctx.request.query['keyword']) {
       const keyword = String(ctx.request.query['keyword'])
-      query = query.where((it) => {it.where('result', 'like', `%${keyword}%`).orWhere('remark', 'like', `%${keyword}%`)})
-    }
-    ctx.body = await query.orderBy('id', 'desc')
-  })
-
+      query = query.where((it) => {it.where('announcement.topic','like', `%${keyword}%`).orWhere('announcement.description','like',`%${keyword}%`)})
+  }
+  ctx.body = await query.orderBy('id','desc')
+})
 
 export default router
-
